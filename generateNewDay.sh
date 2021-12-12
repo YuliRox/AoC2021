96086a6466e7e89705cc9b7eefb49c7a2db9ad9a
Sandra Schuhmacher <sandra.schuhmacher@sva.de>
2021-12-12 15:33:26 +0100

    added script to create structure for daily puzzle

---
+++ generateNewDay.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+NEWDAY=$1
+
+cp -r template $NEWDAY
---
+++ template/src/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputData = Directory.GetFiles(@"./input", "*.txt")
+                .Select(filePath => new FileInfo(filePath))
+                .Where(fileInfo => fileInfo.Name.Contains('_'))
+                .Select(fileInfo => new
+                {
+                    Name = Path.GetFileNameWithoutExtension(fileInfo.Name)
+                        .Split('_')
+                        .Last(),
+                    Content = File.ReadAllLines(fileInfo.FullName)
+                        .Select(int.Parse)
+                        .ToList()
+                });
+
+        }
+    }
+}
