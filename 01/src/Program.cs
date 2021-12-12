80568574fa51e4c25e5e8e03e00fd8194749723d
Sandra Schuhmacher <sandra.schuhmacher@sva.de>
2021-12-12 15:20:25 +0100

    solved part 2

--- 01/src/Program.cs
+++ 01/src/Program.cs
@@ -30,6 +30,14 @@ namespace src
                 Console.WriteLine($"Depth increased a total of {depthIncrease} times");
 
                 Console.WriteLine($"< Part-1 for {input.Name}");
+
+
+                Console.WriteLine($"> Part-2 for {input.Name}");
+
+                var depthIncrease2 = Part2(input.Content);
+                Console.WriteLine($"Depth increased {depthIncrease2} times in a window of 3");
+
+                Console.WriteLine($"< Part-2 for {input.Name}");
             }
         }
 
@@ -39,6 +47,27 @@ namespace src
                                                           .Take(currentList.Count);
             return currentList.Zip(successorList, (current, successor) => successor < current ? 1 : 0)
                                            .Sum();
+        }
+
+        private static int Part2(List<int> inputData)
+        {
+            var windowedData = inputData
+                // append index to values
+                .Zip(Enumerable.Range(0, inputData.Count),
+                    (value, index) =>
+                        new { Index = index, Value = value })
+                // select sub-lists of data
+                .Select(pair => inputData
+                    .Skip(pair.Index)
+                    .Take(3) // with a length of maximum 3
+                    .ToArray())
+                // filter sets with less than 3 elements
+                .Where(result => result.Length == 3)
+                // sum their result
+                .Select(result => result.Sum())
+                .ToList();
+
+            return Part1(windowedData);
         }
     }
 }
