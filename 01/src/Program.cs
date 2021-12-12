f7642c7986e5d9cd4a37482e09a0c35c7ed2f800
Sandra Schuhmacher <sandra.schuhmacher@sva.de>
2021-12-12 15:05:19 +0100

    solved part 1

--- 01/src/Program.cs
+++ 01/src/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace src
 {
@@ -6,7 +9,36 @@ namespace src
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var inputData = Directory.GetFiles(@"./input", "*.txt")
+                .Select(filePath => new FileInfo(filePath))
+                .Where(fileInfo => fileInfo.Name.Contains('_'))
+                .Select(fileInfo => new
+                {
+                    Name = Path.GetFileNameWithoutExtension(fileInfo.Name)
+                        .Split('_')
+                        .Last(),
+                    Content = File.ReadAllLines(fileInfo.FullName)
+                        .Select(int.Parse)
+                        .ToList()
+                });
+
+            foreach (var input in inputData)
+            {
+                Console.WriteLine($"> Part-1 for {input.Name}");
+
+                var depthIncrease = Part1(input.Content);
+                Console.WriteLine($"Depth increased a total of {depthIncrease} times");
+
+                Console.WriteLine($"< Part-1 for {input.Name}");
+            }
+        }
+
+        private static int Part1(List<int> currentList)
+        {
+            var successorList = (new[] { currentList.First() }).Concat(currentList)
+                                                          .Take(currentList.Count);
+            return currentList.Zip(successorList, (current, successor) => successor < current ? 1 : 0)
+                                           .Sum();
         }
     }
 }
