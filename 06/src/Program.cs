b7ce9cf87756e1b90fa12c7fda12346b99b6d5ed
Sandra Schuhmacher <sandra.schuhmacher@sva.de>
2022-08-17 14:58:54 +0200

    Day 6, Template

---
+++ 06/src/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Shared;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var inputData = DataLoader.LoadInputData();
+
+            foreach (var inputSet in inputData)
+            {
+                Console.WriteLine($"> Part-1 for {inputSet.Name}");
+
+                var value = Part1(inputSet);
+
+                Console.WriteLine($"< Part-1 for {inputSet.Name}");
+
+
+                Console.WriteLine($"> Part-2 for {inputSet.Name}");
+
+                var value2 = Part2(inputSet);
+
+                Console.WriteLine($"< Part-2 for {inputSet.Name}");
+            }
+        }
+
+        private static int Part1(PuzzleInput<string> input)
+        {
+            return 0;
+        }
+
+        private static int Part2(PuzzleInput<string> input)
+        {
+            return 0;
+        }
+    }
+}
