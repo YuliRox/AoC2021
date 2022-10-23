af4d30a700471fc8f2da1831561bbf83760b85c8
Sandra Schuhmacher <coding@voiding-warranties.de>
2022-10-23 15:47:14 +0200

    added day 7, tweaked generation script

--- generateNewDay.sh
+++ generateNewDay.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 NEWDAY=$1
+PROJECT_NAME=$2
+
+if [ -z $1 ]; then
+	echo "Please Enter a Day Number"
+	exit
+fi
+
+if [ -z $2 ]; then
+	echo "Please Enter a Project Name"
+	exit
+fi
 
 cp -r template $NEWDAY
+mv $NEWDAY/src $NEWDAY/$2
+mv $NEWDAY/$2/src.csproj $NEWDAY/$2/$2.csproj
+dotnet sln add $NEWDAY/$2/$2.csproj
